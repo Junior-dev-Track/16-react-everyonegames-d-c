cf36fb108c7addf897a703395482d2a0d9d23415
artisan24fullstack <denisartisanfullstack@gmail.com>
2024-04-25 11:34:54 +0200

    prototype

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../layout/Header.jsx';
-import SearchBar from '../layout/SearchBar';
+import SearchBar from '../layout/SearchBar.jsx';
 import NewRelease from '../layout/NewRelease.jsx';
-//import NewRelease from "../../Components/HomePage/NewRelease/NewRelease.jsx";
-//import TopGames from "../../Components/HomePage/TopGames/TopGames.jsx";
-//import EpicGamesHeader from "../../Components/Header/Header.jsx";
-//import EpicGamesSearchBar from "../../Components/SearchBar/SearchBar.jsx";
-//import GamesCarousel from "../../Components/HomePage/GamesCarousel/GamesCarousel.jsx";
 
 function HomePage() {
 
